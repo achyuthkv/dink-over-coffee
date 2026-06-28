8b74be5b366d5e4cc97402572b514a05d79b9a1e
Achyuth Koneri V <37411805+achyuthkv@users.noreply.github.com>
2026-06-28 21:58:26 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App.jsx'
+import AdminLayout from './admin/AdminLayout.jsx'
+import ResetPassword from './admin/ResetPassword.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/admin/reset" element={<ResetPassword />} />
+        <Route path="/admin/*" element={<AdminLayout />} />
+        <Route path="*" element={<App />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 )
